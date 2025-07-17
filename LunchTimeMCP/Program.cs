78c0124f16d1a7774ba073860f494ab4bc743cf0
Bruno Capuano <bcapuano@gmail.com>
2025-07-17 11:55:02 -0400

    Add LunchTimeMCP project with restaurant management features and tools

---
+++ LunchTimeMCP/Program.cs
@@ -0,0 +1,15 @@
+﻿using LunchTimeMCP;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ModelContextProtocol.Server;
+
+var builder = Host.CreateEmptyApplicationBuilder(settings: null);
+
+builder.Services
+    .AddMcpServer()
+    .WithStdioServerTransport()
+    .WithTools<RestaurantTools>();
+
+builder.Services.AddSingleton<RestaurantService>();
+
+await builder.Build().RunAsync();
